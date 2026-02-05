b19ceb4319a775ff346ff45924602ef55d217188
Valeh Huseynov <valehmh@code.edu.az>
2026-02-05 14:46:43 +0400

    Students Controllers

--- app/strudents/students.controller.ts
+++ app/strudents/students.controller.ts
@@ -1,18 +1,42 @@
-import { Controller ,Get,Post,Body} from '@nestjs/common';
+import { Controller ,Get,Post,Body,Put,Param,Delete,HttpCode,NotFoundException} from '@nestjs/common';
+import {Response} from "express";
 import {StudentsService} from "./students.service";
 import {IStudent} from "./interfaces/strudent.interface";
-
 @Controller('students')
 export class StudentsController {
-
+    private students: IStudent[] = [{id: 1,name: "Example name", age: 55}];
     constructor(private studentsServices: StudentsService) {}
+
     @Get()
     findAll(): IStudent[] {
-        return this.studentsServices.findAll();
+        return this.students;
+    }
+
+    @Get(':id')
+    @HttpCode(200)
+    findOne(@Param('id') id: string) {
+        const student = this.students.find(s => s.id === +id);
+        if(!student) {
+            throw new NotFoundException(`Student with ID ${id} not found`);
+        }
+        console.log(student);
+        return student;
     }
 
     @Post()
+    @HttpCode(202)
     create(@Body() student: IStudent) {
-       this.studentsServices.create(student);
+       this.students.push(student);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string,@Body() body: IStudent): string {
+        console.log(id,body);
+        return `This will update student with ID ${id} with the following data: ${JSON.stringify(body)}`;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): string {
+        return `This will delete student with ID ${id}`;
     }
 }
